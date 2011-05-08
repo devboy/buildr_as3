18f5311144747ac6a678cfe05858d30cde80c486
Dominic Graefen <devboy@Dominic-Graefens-MacBook-Pro.local>
2011-05-08 11:52:31 +0200

    Fixed a bug in compiler.needed? which determines if a project needs to be compiled.

--- lib/buildr/as3/compiler/base.rb
+++ lib/buildr/as3/compiler/base.rb
@@ -60,7 +60,7 @@ module Buildr
         def needed?(sources, target, dependencies)
           return true unless File.exist?(@project.get_as3_output(target, options))
           Dir.glob(FileList[sources,dependencies].to_a.map{ |file| file = "#{file}/**/*" } ).
-              map{|file| File.stat(file).mtime}.max > File.stat(@project.get_as3_output(target, options)).mtime
+              map{|file| File.stat(file).mtime}.max || Time.at(0) > File.stat(@project.get_as3_output(target, options)).mtime
         end
 
         private
