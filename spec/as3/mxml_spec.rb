ef1eb61f296702b099cef7cfd48efbf73f49290c
devboy <dominic.graefen@gmail.com>
2011-09-22 23:08:57 +0200

    First batch of real mxmlc tests.

--- spec/as3/mxml_spec.rb
+++ spec/as3/mxml_spec.rb
@@ -17,13 +17,30 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helpers'))
 
 
-describe 'javac compiler' do
-  it 'should identify itself from source directories' do
-    write 'src/main/as3/com/example/Test.java', 'package com.example; class Test {}'
-    #define('foo').compile.compiler.should eql(:javac)
-    define('foo') do
-      compile.using :mxmlc
-    end.compile.compiler.should eql(:mxmlc)
+describe Buildr::AS3::Compiler::Mxmlc do
+
+  it 'should report the language as :actionscript' do
+    define('foo').compile.using(:mxmlc).language.should eql(:actionscript)
+  end
+
+  it 'should set the target directory to target/bin' do
+    define 'foo' do
+      lambda { compile.using(:mxmlc) }.should change { compile.target.to_s }.to(File.expand_path('target/bin'))
+    end
+  end
+
+  it 'should not override existing target directory' do
+    define 'foo' do
+      compile.into('classes')
+      lambda { compile.using(:mxmlc) }.should_not change { compile.target }
+    end
+  end
+
+  it 'should not change existing list of sources' do
+    define 'foo' do
+      compile.from('sources')
+      lambda { compile.using(:mxmlc) }.should_not change { compile.sources }
+    end
   end
 
   after do
