203b66269eaa27b8d8be3c1138fa924b65ab914f
devboy <dominic.graefen@gmail.com>
2011-09-24 16:39:10 +0200

    Added prerelease rake tasks.

--- rake/jeweler.rb
+++ rake/jeweler.rb
@@ -1,13 +1,13 @@
 class Jeweler
-  autoload :PreReleaseToGit, File.dirname(__FILE__)+'/pre_release_to_git'
-  autoload :PreReleaseGemspec, File.dirname(__FILE__)+'/pre_release_gemspec'
+  require File.dirname(__FILE__)+'/pre_release_to_git'
+  require File.dirname(__FILE__)+'/pre_release_gemspec'
 
   def prerelease_to_git
-  Jeweler::Commands::PreReleaseToGit.build_for(self).run
+    Jeweler::Commands::PreReleaseToGit.build_for(self).run
   end
 
   def prerelease_gemspec
-  Jeweler::Commands::PreReleaseGemspec.build_for(self).run
+    Jeweler::Commands::PreReleaseGemspec.build_for(self).run
   end
 
   def is_prerelease_version?
