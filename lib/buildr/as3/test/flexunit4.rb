3ebf401474f7f3df46c981a8ca35bd9f356732b5
devboy <dominic.graefen@gmail.com>
2011-11-01 17:15:04 +0100

    adds :port option to flexunit4

--- lib/buildr/as3/test/flexunit4.rb
+++ lib/buildr/as3/test/flexunit4.rb
@@ -105,7 +105,7 @@ module Buildr
                          :localTrusted => options[:localTrusted] || true,
                          :headless => options[:headless] || false,
                          :display => options[:display] || 99,
-                         :port => options[:port] || 1028,
+                         :port => options[:port] || 1024,
                          :swf => task.project.get_as3_output(true)
 
             ant.taskdef :name=>'junitreport',
